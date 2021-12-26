d80e417e464a99b368a08f8524722e03be37b456
pardeep729 <6729957+pardeep729@users.noreply.github.com>
2021-12-26 17:23:23 +0000

    IO problem + threading soln

---
+++ io problem - threading solution.py
@@ -0,0 +1,34 @@
+import requests
+import time
+
+import threading
+from concurrent.futures import ThreadPoolExecutor
+
+thread_local = threading.local()
+
+def get_session():
+    if not hasattr(thread_local, "session"):
+        thread_local.session = requests.Session()
+    return thread_local.session
+
+def download_site(idx, url):
+    session = get_session()
+    with session.get(url) as response:
+        print(f"{idx}: Read {len(response.content)} from {url}")
+
+
+def download_all_sites(sites):
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(download_site, sites.keys(), sites.values())
+
+
+if __name__ == "__main__":
+    sites = [
+        "https://www.jython.org",
+        "http://olympus.realpython.org/dice",
+    ] * 250
+    sites = {idx: s for idx, s in enumerate(sites)}
+    start_time = time.time()
+    download_all_sites(sites)
+    duration = time.time() - start_time
+    print(f"Downloaded {len(sites)} in {duration} seconds")
---
+++ io problem.py
@@ -0,0 +1,24 @@
+import requests
+import time
+
+def download_site(idx, url, session):
+    with session.get(url) as response:
+        print(f"{idx}: Read {len(response.content)} from {url}")
+
+
+def download_all_sites(sites):
+    with requests.Session() as session:
+        for idx, url in sites.items():
+            download_site(idx, url, session)
+
+
+if __name__ == "__main__":
+    sites = [
+        "https://www.jython.org",
+        "http://olympus.realpython.org/dice",
+    ] * 50
+    sites = {idx: s for idx, s in enumerate(sites)}
+    start_time = time.time()
+    download_all_sites(sites)
+    duration = time.time() - start_time
+    print(f"Downloaded {len(sites)} in {duration} seconds")
